71b019217e44fb040b5cab954cf15d368bb1de85
jaikrishnaverma <jaikrishnaverma>
2022-08-08 11:15:57 +0530

    updated

--- product.js
+++ product.js
@@ -35,6 +35,7 @@ var products = [{
    "brand": "Microsoft",
    "os": "Windows"
 }];
+var cmnd = 0;
 populate(products);
 
 $("#tbody1").on("click", "#remove", function () {
@@ -42,23 +43,42 @@ $("#tbody1").on("click", "#remove", function () {
    $row.hide();
 });
 
-$("#filter1").change(function () {
-   var sort_by = $(this).val();
+$("#headtr").on("change", "select", function () {
+   var forthis = $(this).attr("class");
+   var value = $(this).val();
    // console.log(sort_by);
-   products = products.sort(function (a, b) { let x = a[sort_by].toLowerCase(); let y = b[sort_by].toLowerCase(); if (x < y) { return -1; } if (x > y) { return 1; } return 0; });
-   populate(products);
+   // products = products.sort(function (a, b) { let x = a[sort_by].toLowerCase(); let y = b[sort_by].toLowerCase(); if (x < y) { return -1; } if (x > y) { return 1; } return 0; });
+   // populate(products);
+   var newArray = products.filter(function (el) {
+      return el[forthis] == value;
+
+   });
+   populate(newArray);
+   // console.log(newArray);
 });
+
+
+
 $('#search1').click(function () {
    let value = $('#input1').val().toLowerCase();
-   $("#tbody1 tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-   })
+   var newArray = products.filter(function (el) {
+      return (el.id == value || el.name.toLowerCase() == value);
+
+   });
+   // $("#tbody1 tr").filter(function () {
+   //    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+   // })
+   populate(newArray);
 })
 
 function populate(arr) {
    var row = "";
+   var brand = [];
+   var os = [];
    for (let i = 0; i < arr.length; i++) {
       row += '<tr>';
+      brand.push(arr[i].brand);
+      os.push(arr[i].os);
       $.each(arr[i], function (key, value) {
 
          row += '<td id="' + key + '">' + value + '</td>';
@@ -66,4 +86,21 @@ function populate(arr) {
       row += '<td id="remove">X</td></tr>';
    }
    $('#tbody1').html(row);
+   // console.log(os);
+   if (cmnd == 0) {
+      brandArr = brand.filter((v, i, a) => a.indexOf(v) === i);
+      osArr = os.filter((v, i, a) => a.indexOf(v) === i);
+      console.log(brandArr);
+      $("#filter1").append(optioner(brandArr));
+      $("#filter2").append(optioner(osArr));
+      cmnd++;
+   }
+
+}
+function optioner(arr) {
+   let tem = "";
+   for (let i = 0; i < arr.length; i++) {
+      tem += '<option value=' + arr[i] + '>' + arr[i] + '</option>';
+   }
+   return tem;
 }
